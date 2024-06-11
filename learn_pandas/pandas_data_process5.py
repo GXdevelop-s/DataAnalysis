50e9ab6db72af24c6f67ce8652d2ec7447d1a5cd
gaoxu <xgao53543@gmail.com>
2024-06-11 11:21:07 +0800

    pandas 数据分组和分组后聚合操作

---
+++ learn_pandas/pandas_data_process5.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+
+if __name__ == '__main__':
+    # 1.数据分组聚合
+    '''
+    数据聚合是数据处理的最后一步，通常是要使每一个数组生成一个单一的数值
+    数据分类处理：
+        分组：先把数据分成几组
+        用函数处理：为不同组的数据应用不同的函数以转换数据
+        合并：把不同组得到的结果合并起来
+    '''
+    df1 = pd.DataFrame({
+        'color': ['red', 'blue', 'green', 'green', 'red', 'red', 'yellow'],
+        'price': [3, 7, 1, 9, 3, 7, 8]
+    })
+    # 按color进行分组
+    grouped = df1.groupby(
+        by='color')  # 返回的是一个DataFrameGroupBy对象，这个对象本身并不是一个DataFrame，而是一个包含多个组的中间对象，每个组都是原始df分组列值相同的那些行。
+    print(grouped)  # 结果是<pandas.core.groupby.generic.DataFrameGroupBy object at 0x103097070>
+    print(grouped.groups)  # groups用来查看分组情况，目前是每个color中所含的index值，因为对price还没有做处理
+    # 遍历每个分组
+    for name, group in grouped:
+        # print(name)
+        # print(group)    # 这是个df，说明遍历GroupBy对象，每个元素是一个dataframe
+        pass
+    # 分组+聚合
+    r1 = df1.groupby(by='color').sum()  # 按照color分组，对price求和，因为只有price有值，返回值是一个dataframe
+    # 练习
+    # 1对ddd进行聚合操作，求出颜色为白色的价格总和
+    # 2对ddd进行聚合操作，分别求出萝卜的所有重量和平均价格
+    # 3使用merge合并总重量和平均价格
+    ddd = pd.DataFrame(
+        data={
+            'item': ["萝卜", "白菜", "辣椒", "冬瓜", "萝卜", "白菜", "辣椒", "冬瓜"],
+            'color': ["白", "青", "红", "白", "青", "红", "白", "青"],
+            'weight': [10, 20, 10, 10, 30, 40, 50, 60],
+            'price': [0.99, 1.99, 2.99, 3.99, 4, 5, 6, 7]
+        }
+    )
+    # 1  解法1:分组后再按price求和  解法2:查询出所有白色的行，再聚合
+    s_group_ddd = ddd.groupby(by='color')['price']  # DataframeGroupBy取单列是SeriesGroupBy
+    d_group_ddd = ddd.groupby(by='color')[['price']]  # DataframeGroupBy取多列是DataframeGroupBy
+    white_total = s_group_ddd.sum()['白']  # XxxGroupBy对象聚合之后就会变成Xxx
+    print(white_total)
+
+    # 2
+    carrot_weight = ddd.groupby(by='item')[['weight']].sum()
+    print(carrot_weight)
+    carrot_avg_price = ddd.groupby(by='item')[['price']].mean()
+    print(carrot_avg_price)
+
+    # 3
+    merge_r = carrot_weight.merge(carrot_avg_price, left_index=True, right_index=True)
+    print(merge_r)
--- learn_pandas/test.py
+++ learn_pandas/test.py
@@ -2,16 +2,11 @@ import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
-    df13 = pd.DataFrame(data=np.random.randint(0, 100, size=(5, 3)), index=list('ABCDE'), columns=['PY', 'NP', 'PD'])
-    print(df13)
-
-    def fun1(x):
-        x['PY'] += 1
-        x['NP'] -= 1
-        x['PD'] *= 10
-
-
-    df13.apply(fun1,axis=1)
-    print(df13)
-    print(df13.info)
-
+    df1 = pd.DataFrame({
+        'color': ['red', 'blue', 'green', 'green', 'red', 'red', 'yellow'],
+        'price': [3, 7, 1, 9, 3, 7, 8]
+    })
+    grouped = df1.groupby(by='color')
+    for name, group in grouped:
+        print(name)
+        print(group)
